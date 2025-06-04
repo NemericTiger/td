8e965352d2a1834d3134977149358e6e1837b1b9
NemericTiger <Paul.l.messerschmidt@gmail.com>
2025-06-04 22:53:00 +0200

    better structure, first turret

--- mobs/soldier_a.gd
+++ mobs/soldier_a.gd
@@ -4,5 +4,5 @@ extends CharacterBody2D
 
 func _process(delta: float):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
-	if get_parent().get_progress_ratio() == 1:
+	if get_parent().get_progress_ratio() == 0.99:
 		queue_free()
