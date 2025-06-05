c3630d2c1035a27f714a39034a86a473e545ebaf
NemericTiger <Paul.l.messerschmidt@gmail.com>
2025-06-05 20:11:05 +0200

    first turret

--- mobs/soldier_a.gd
+++ mobs/soldier_a.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-@export var speed = 300
+@export var speed = 200
+var Health = 10
 
 func _process(delta: float):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 0.99:
 		queue_free()
+	elif Health <= 0:
+		get_parent().get_parent().queue_free()
