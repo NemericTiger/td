84e21c5051f9b87d29b4b0e731c90453b0b990a5
NemericTiger <Paul.l.messerschmidt@gmail.com>
2025-06-05 22:29:01 +0200

    bug fixes

--- mobs/soldier_a.gd
+++ mobs/soldier_a.gd
@@ -5,7 +5,7 @@ var Health = 10
 
 func _process(delta: float):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
-	if get_parent().get_progress_ratio() == 0.99:
-		queue_free()
+	if get_parent().get_progress_ratio() >= 0.99:
+		get_parent().get_parent().queue_free()
 	elif Health <= 0:
 		get_parent().get_parent().queue_free()
--- towers/red_tower_panel.gd
+++ towers/red_tower_panel.gd
@@ -10,14 +10,22 @@ func _on_gui_input(event: InputEvent) -> void:
 		add_child(tempTower)
 		#tempTower.global_position = event.global_position
 		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
-	if event is InputEventMouseMotion and event.button_mask == 1:
+		tempTower.scale = Vector2(0.32, 0.32)
+	elif event is InputEventMouseMotion and event.button_mask == 1:
 		get_child(1).global_position = event.global_position
 		
 	elif event is InputEventMouseButton and event.button_mask == 0:
-		print("mb1 unclick")
-		get_child(1).queue_free()
-		var path = get_tree().get_root().get_node("Main")
-		
-		path.add_child(tempTower)
-		tempTower.global_position = event.global_position
-		tempTower.get_node("Area").hide()
+		if event.global_position.x >= 2944:
+			if get_child_count() > 1:
+				get_child(1).queue_free()
+		else:
+			if get_child_count() > 1:
+				get_child(1).queue_free()
+			var path = get_tree().get_root().get_node("Main")
+			
+			path.add_child(tempTower)
+			tempTower.global_position = event.global_position
+			tempTower.get_node("Area").hide()
+	else:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
